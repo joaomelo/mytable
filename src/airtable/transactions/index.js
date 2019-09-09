5e4e825ddd8eb8e084f0595ad24237f9b6bd6a7b
joao <j@vmelo.net>
2019-09-09 17:53:52 -0300

    fixed bug with zero job transactions sums

--- src/airtable/transactions/index.js
+++ src/airtable/transactions/index.js
@@ -7,66 +7,65 @@ function createJobTransactionsCommand(job, snapshot) {
   let command;
 
   if (!calcJobError(job, snapshot)) {
-    const transactions = snapshot.getTransactions(job, true);
-    if (transactions && transactions.length > 0) {
-      const entries = calcAverageAcc(transactions);
+    const entries = calcAverageAcc(job, snapshot);
 
-      Object.keys(entries).forEach(k => {
-        if (entries[k] == job[k]) {
-          delete entries[k];
-        }
-      });
+    Object.keys(entries).forEach(k => {
+      const original = job[k] === undefined ? '' : job[k];
+      const calculated = entries[k] === 0 ? '' : entries[k].toFixed(2);
 
-      if (Object.keys(entries).length > 0) {
-        command = {
-          type: 'update',
-          table: 'jobs',
-          id: job.id,
-          tag: job.title,
-          entries: entries
-        };
+      if (calculated == original) {
+        delete entries[k];
+      } else {
+        entries[k] = calculated;
       }
+    });
+
+    if (Object.keys(entries).length > 0) {
+      command = {
+        type: 'update',
+        table: 'jobs',
+        id: job.id,
+        tag: job.title,
+        entries: entries
+      };
     }
   }
 
   return command;
 }
 
-function calcAverageAcc(transactions) {
-  const today = moment().endOf('day');
-
-  const subtract = date =>
-    moment()
-      .startOf('day')
-      .subtract(1, date);
-  const monthAgo = subtract('months');
-  const quarterAgo = subtract('quarters');
-  const yearAgo = subtract('years');
-
-  const averageAcc = {
+function calcAverageAcc(job, snapshot) {
+  const avg = {
     month: 0,
     quarter: 0,
     year: 0
   };
 
-  transactions.forEach(t => {
-    if (moment(t.date).isSameOrBefore(today)) {
-      if (moment(t.date).isSameOrAfter(yearAgo)) {
-        averageAcc.year += t.value / 12;
-        if (moment(t.date).isSameOrAfter(quarterAgo)) {
-          averageAcc.quarter += t.value / 3;
-          if (moment(t.date).isSameOrAfter(monthAgo)) {
-            averageAcc.month += t.value;
+  const transactions = snapshot.getTransactions(job, true);
+  if (transactions && transactions.length > 0) {
+    const today = moment().endOf('day');
+
+    const subtract = date =>
+      moment()
+        .startOf('day')
+        .subtract(1, date);
+    const monthAgo = subtract('months');
+    const quarterAgo = subtract('quarters');
+    const yearAgo = subtract('years');
+
+    transactions.forEach(t => {
+      if (moment(t.date).isSameOrBefore(today)) {
+        if (moment(t.date).isSameOrAfter(yearAgo)) {
+          avg.year += t.value / 12;
+          if (moment(t.date).isSameOrAfter(quarterAgo)) {
+            avg.quarter += t.value / 3;
+            if (moment(t.date).isSameOrAfter(monthAgo)) {
+              avg.month += t.value;
+            }
           }
         }
       }
-    }
-  });
-
-  Object.keys(averageAcc).forEach(
-    k =>
-      (averageAcc[k] =
-        averageAcc[k] == 0 ? undefined : averageAcc[k].toFixed(2))
-  );
-  return averageAcc;
+    });
+  }
+  return avg;
 }
